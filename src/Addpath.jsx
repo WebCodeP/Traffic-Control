051fb478d1a3ddf7cbc9cade2c0b946e12b75f7b
jafar <jafarhorane3000@gmail.com>
2024-03-16 14:02:19 -0700

    addmarkere

--- src/Addpath.jsx
+++ src/Addpath.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState ,useRef} from 'react';
 import './addpath.css';
 import { MapContainer, TileLayer ,Marker} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -10,28 +10,30 @@ import { Icon } from 'leaflet';
 
 const Addpath = () => {
 
-
-const [place , setPlace] = useState(null);
+const mapRef = useRef(null);
+const [point ,setPoint ] = useState([]);
 // icon
 const customIcon = new Icon({
   iconUrl:site,
   iconSize: [32, 32],
   });
 //end icon
-
-function addPoint()
-{
-  console.log("hhhdhhdhd");
+if(mapRef.current){
+  mapRef.current.on("click",(event)=>{
+     const newArray = [event.latlng.lat,event.latlng.lng];
+     setPoint([...point,newArray]);
+  });
 }
 
 
   return (
     <div className='add_path_container'>
-        <MapContainer  style={{ height: "100%", width: "100%" }}  center={[34.7325,36.7367]} zoom={13} zoomControl={false} onClick={addPoint}>
+        <MapContainer ref={mapRef}  style={{ height: "100%", width: "100%" }}  center={[34.7325,36.7367]} zoom={13} zoomControl={false} >
               <TileLayer   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
               {/* <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" attribution="Esri"/> */}
 
-             {place && <Marker className="type_car" position={place} icon={customIcon}></Marker>}
+             {point && point.map((place)=>(<Marker className="type_car" position={place} icon={customIcon}></Marker> ))}
+        
         </MapContainer>
         <Layoutadd/>
     </div>
