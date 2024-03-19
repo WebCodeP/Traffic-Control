ac910bf3ec26f854a29d5b2f284daaf106fea953
jafar <jafarhorane3000@gmail.com>
2024-03-19 06:30:27 -0700

    show

--- src/ButtonAdd.jsx
+++ src/ButtonAdd.jsx
@@ -4,14 +4,19 @@ import "./button.css";
 import { GiPathDistance } from "react-icons/gi";
 import { MdDirectionsBusFilled } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 const ButtonAdd = () => {
+const [show , setShow] = useState("none");
+const showMenu = () =>{
+  setShow("block")
+}
   return (
 
     <div className="add_button">
-       <IoMdAddCircleOutline className='add_line'/>
-       <div className="menu">
+       <IoMdAddCircleOutline className='add_line' onClick={showMenu}/>
+       <div className="menu" style={{display:show}}>
         
          <Link to="/Traffic-Control/AddPath" className="add_line">
             <span>مسار</span>
